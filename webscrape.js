b1725fb7bc13306c2798420acd2e67aab3fa3a0c
Matt Clewley <mattclew21@gmail.com>
2020-04-10 16:35:00 -0400

    test with json url
    tesing with wordpress json url

--- webscrape.js
+++ webscrape.js
@@ -1,1 +1,7 @@
 console.log("hello world");
+
+(async () => {
+    const response = await fetch('https://wordpress.org/wp-json');
+    const json = await response.json();
+    console.log(JSON.stringify(json));
+})()
