e8169e1b6c18cf7bb5787d64e684496ebb377c92
Matt Clewley <mattclew21@gmail.com>
2020-04-11 10:25:53 -0400

    testing my site

--- webscrape.js
+++ webscrape.js
@@ -1,7 +1,15 @@
 console.log("hello world");
 
+/* getting from json url
 (async () => {
     const response = await fetch('https://wordpress.org/wp-json');
     const json = await response.json();
     console.log(JSON.stringify(json));
 })()
+*/
+
+(async () => {
+    const response = await fetch('https://mattclewley.imfast.io/');
+    const text = await response.text();
+    console.log(text.match(/(?<=\<h1>).*(?=\<\/h1>)/));
+  })()
