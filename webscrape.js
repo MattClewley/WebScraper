cf151b44babc65eac69977319b9083f2bf1e73da
Matt Clewley <mattclew21@gmail.com>
2020-04-12 19:08:00 -0400

    updated the target url

--- webscrape.js
+++ webscrape.js
@@ -9,7 +9,7 @@ console.log("hello world");
 */
 
 (async () => {
-    const response = await fetch('https://mattclewley.imfast.io/');
+    const response = await fetch('http://quotes.toscrape.com/');
     const text = await response.text();
     console.log(text.match(/(?<=\<h1>).*(?=\<\/h1>)/));
   })()
