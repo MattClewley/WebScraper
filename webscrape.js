b735cb56efd28d233ada16dd34b8cac42d6a05a9
Matt Clewley <mattclew21@gmail.com>
2020-04-13 10:12:29 -0400

    moved script from js file to html

--- webscrape.js
+++ webscrape.js
@@ -1,15 +1,15 @@
 console.log("hello world");
 
-/* getting from json url
-(async () => {
+//getting from json url
+/*(async () => {
     const response = await fetch('https://wordpress.org/wp-json');
     const json = await response.json();
     console.log(JSON.stringify(json));
-})()
-*/
+})*/
 
-(async () => {
+
+/*(async () => {
     const response = await fetch('http://quotes.toscrape.com/');
     const text = await response.text();
     console.log(text.match(/(?<=\<h1>).*(?=\<\/h1>)/));
-  })()
+  })()*/
